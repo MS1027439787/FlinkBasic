8f2f6cf5997d09d1da0f9b30dd95cb423d0c37eb
masai <masai@95303.com>
2021-03-15 10:59:20 +0800

    second

---
+++ src/main/java/BasicTestJava.java
@@ -0,0 +1,45 @@
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @author masai
+ * @date 2021/3/15
+ */
+public class BasicTestJava {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Person> flintstones = env.fromElements(
+                new Person("Fred", 35),
+                new Person("Wilma", 35),
+                new Person("Pebbles", 2));
+
+        DataStream<Person> adults = flintstones.filter(new FilterFunction<Person>() {
+            public boolean filter(Person person) throws Exception {
+                return person.age >= 18;
+            }
+        });
+
+        adults.print();
+
+        env.execute();
+    }
+
+    public static class Person {
+        public String name;
+        public Integer age;
+        public Person() {};
+
+        public Person(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String toString() {
+            return this.name.toString() + ": age " + this.age.toString();
+        }
+    }
+}
