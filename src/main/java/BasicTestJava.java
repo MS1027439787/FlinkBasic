6eb04ea09df7cf81ea7425d0d7264d1d3d365120
masai <masai@95303.com>
2021-03-15 17:36:32 +0800

    third

--- src/main/java/BasicTestJava.java
+++ src/main/java/BasicTestJava.java
@@ -3,6 +3,9 @@ import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author masai
  * @date 2021/3/15
@@ -17,6 +20,16 @@ public class BasicTestJava {
                 new Person("Wilma", 35),
                 new Person("Pebbles", 2));
 
+        //也可以通过fromCollection(Collection)获取数据流
+//        List<Person> people = new ArrayList<Person>();
+//        people.add(new Person("Fred", 35));
+//        people.add(new Person("Wilma", 35));
+//        people.add(new Person("Pebbles", 2));
+//        DataStream<Person> flintstones = env.fromCollection(people);
+        //另一个获取数据到流中的便捷方法是用 socket
+        //DataStream<String> lines = env.socketTextStream("localhost", 9999)
+        //或读取文件
+        //DataStream<String> lines = env.readTextFile("file:///path");
         DataStream<Person> adults = flintstones.filter(new FilterFunction<Person>() {
             public boolean filter(Person person) throws Exception {
                 return person.age >= 18;
